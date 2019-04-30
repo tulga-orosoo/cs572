b841a1a166fc91a8b4dd2bef2e4709166bfa7376
Tulga Orosoo <tulga.orosoo@gmail.com>
2019-04-30 03:01:49 -0500

    move javascript code to external js ffile

--- Codes/homework_01/exercise1.js
+++ Codes/homework_01/exercise1.js
@@ -1,5 +1,4 @@
 
-
 // String.prototype.filterWords = function(strArray) {
 //   return this.valueOf().split(' ').map(str => {
 //     if(strArray.indexOf(str) > -1) {
@@ -41,22 +40,52 @@ String.prototype.filterWords = async function(strArray) {
   });
 }
 
-
-console.log(await "This house is nice!".filterWords(['house', 'nice']));
-
-
-
+let promise = "This house is nice!".filterWords(['house', 'nice']);
+let result = /*await*/ promise;
+console.log(result);
 
 
-String.prototype.filterWords = async function(strArray) {
+const {Observable} = rxjs;
+
+String.prototype.filterWords = function(strArray) {
   let originalStr = this.valueOf();
-  return new Promise(function(resolve, reject) {
+  return Observable.create(function(observer) {
     strArray.map(str => originalStr = originalStr.replace(str, "***"));
-    resolve(originalStr);
+    observer.next(originalStr);
+    setTimeout(() => { observer.complete(); }, 2000);
   });
 }
 
+const subscriptin = "This house is nice!".filterWords(['house', 'nice']).subscribe(
+  function(x) {console.log(x)},
+  function(err) {console.log(err)},
+  function() {console.log("Observable done!")},
+)
 
-console.log(await "This house is nice!".filterWords(['house', 'nice']));
 
 
+
+function isWeekend() {
+  const todayDate = new Date();
+  const day = todayDate.getDay();
+  const daysName = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend'];
+  return daysName[day];
+}
+
+console.log(isWeekend());
+
+function applyCoupon(item) {
+  return function(ammount) {
+    item.price -= ammount;
+    return item;
+  }
+}
+
+const item = {
+  "name": "Avocado",
+  "type": "Fruit",
+  "category": "Food",
+  "price": 200
+}
+
+console.log(applyCoupon(item)(20).price === 180);
