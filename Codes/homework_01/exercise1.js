d9e15b06a021dc8bedda9ade4c168f5f112670ac
Tulga Orosoo <tulga.orosoo@gmail.com>
2019-04-30 02:37:26 -0500

    exercise 1 done

--- Codes/homework_01/exercise1.js
+++ Codes/homework_01/exercise1.js
@@ -1,38 +1,62 @@
 
+
+// String.prototype.filterWords = function(strArray) {
+//   return this.valueOf().split(' ').map(str => {
+//     if(strArray.indexOf(str) > -1) {
+//       return (new Array(str.length)).join("*");
+//     }
+//     return str;
+//   }).reduce((x, y) => x + ' ' + y);
+// }
+
+// console.log("This house is nice!".filterWords(['house', 'nice']));
+
+
 String.prototype.filterWords = function(strArray) {
   let originalStr = this.valueOf();
-  strArray.map(str => originalStr = originalStr.replace(str, (new Array(str.length)).join("*")));
+  strArray.map(str => originalStr = originalStr.replace(str, "***"));
   return originalStr;
 }
 
 console.log("This house is nice!".filterWords(['house', 'nice']));
 
-String.prototype.filterWords = function(strArray) {
-  return this.valueOf().split(' ').map(str => {
-    if(strArray.indexOf(str) > -1) {
-      return (new Array(str.length)).join("*");
-    }
-    return str;
-  }).reduce((x, y) => x + ' ' + y);
-}
-
-console.log("This house is nice!".filterWords(['house', 'nice']));
 
 String.prototype.filterWords = function(strArray) {
-  const origin = this.valueOf();
-  const promise = async new Promise(function(resolve, reject) {
-    const result = origin.split(' ').map(str => {
-      if(strArray.indexOf(str) > -1) {
-        return (new Array(str.length)).join("*");
-      }
-      return str;
-    }).reduce((x, y) => x + ' ' + y);
-    resolve(result);
-  });
-
-  return await promise.then(function(x) {
-    return x;
+  let originalStr = this.valueOf();
+  return new Promise(function(resolve, reject) {
+    strArray.map(str => originalStr = originalStr.replace(str, "***"));
+    resolve(originalStr);
   });
 }
 
-console.log("This house is nice!".filterWords(['house', 'nice']));
+"This house is nice!".filterWords(['house', 'nice']).then(result => console.log(result));
+
+
+
+String.prototype.filterWords = async function(strArray) {
+  let originalStr = this.valueOf();
+  return new Promise(function(resolve, reject) {
+    strArray.map(str => originalStr = originalStr.replace(str, "***"));
+    resolve(originalStr);
+  });
+}
+
+
+console.log(await "This house is nice!".filterWords(['house', 'nice']));
+
+
+
+
+
+String.prototype.filterWords = async function(strArray) {
+  let originalStr = this.valueOf();
+  return new Promise(function(resolve, reject) {
+    strArray.map(str => originalStr = originalStr.replace(str, "***"));
+    resolve(originalStr);
+  });
+}
+
+
+console.log(await "This house is nice!".filterWords(['house', 'nice']));
+
+
